bd8acff40cd96ba60d14357bb6562ed7a167a5d9
Areeb Khan <areebk@umich.edu>
2024-01-25 18:16:35 -0500

    finished porting ankis

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import os
 import csv
+import genanki
 
+# Edit this with the name of all of your lectures
+lecture_names = [
+    '1 - Introduction',
+    '2 - Processes',
+    '3 - Syncronization: Mutual Exclusion',
+    '4 - Thread-safe queue',
+]
+
+# edit this with what you want to name the csvs (this can be automated, but I prefer control)
+csv_names = [
+    'lecture1.csv',
+    'lecture2.csv',
+    'lecture3.csv',
+    'lecture4.csv',
+]
 
 def read_data(csv_filename): 
     if os.path.exists(csv_filename):
@@ -13,12 +29,48 @@ def read_data(csv_filename):
             for row in reader: 
                 questions.append(tuple(row))
         
-        print(questions)
+        return questions
+
+def parse_into_anki(question_answers, deck_name):
+    my_model = genanki.Model(
+    1607392319,
+    'Simple Model',
+    fields=[
+        {'name': 'Question'},
+        {'name': 'Answer'},
+    ],
+    templates=[
+        {
+        'name': 'Card 1',
+        'qfmt': '{{Question}}',
+        'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+        },
+    ])
+
+    my_deck = genanki.Deck(
+    2059400191,
+    deck_name)
+
+    for pair in question_answers: 
+        notecard = genanki.Note(
+            model = my_model,
+            fields= [pair[0], pair[1]]
+        )
+        my_deck.add_note(notecard)
+
+    output_name = deck_name + '.apkg'
+    genanki.Package(my_deck).write_to_file(output_name)
 
 
 def main():
-    filename = input('Enter the name of the q/a csv')
-    read_data(filename)
+    deck_index = input('What lecture are you on? ')
+    deck_name = lecture_names[int(deck_index) - 1]
+    filename = csv_names[int(deck_index) - 1]
+
+
+    question_answers = read_data(filename)
+    parse_into_anki(question_answers, deck_name)
+
 
 
 
