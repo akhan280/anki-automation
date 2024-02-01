e0b2410d66c52c11480a68e55510cc91f3e63307
Areeb Khan <areebk@umich.edu>
2024-01-31 19:45:21 -0500

    Cleanup

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import genanki
 import io
 import csv
 import pandas as pd
-import numpy as np  # Import numpy here
+import numpy as np 
 
 
 def parse_input(input_data):
-    # Parse the input data from the text area
     reader = csv.reader(io.StringIO(input_data), delimiter=',')
     data = list(reader)
     header = data[0] if data else []  # Capture header
@@ -18,21 +17,17 @@ def display_data(header, data):
         df = pd.DataFrame(data, columns=header)
         st.dataframe(df)
 
-        # Data Summary
         st.subheader("Data Summary")
         st.write(f"Number of Rows: {df.shape[0]}")
         st.write(f"Number of Columns: {df.shape[1]}")
 
-        # Column Data Types
         st.subheader("Column Data Types")
         st.write(df.dtypes)
 
-        # Basic Statistics for Numerical Data
         if df.select_dtypes(include=[np.number]).shape[1] > 0:
             st.subheader("Basic Statistics")
             st.write(df.describe())
 
-        # Data Filtering
         st.subheader("Filter Data")
         col = st.selectbox("Select column to filter", df.columns)
         if df[col].dtype == 'object':
@@ -42,7 +37,6 @@ def display_data(header, data):
             min_value, max_value = st.slider("Select range", df[col].min(), df[col].max(), (df[col].min(), df[col].max()))
             st.dataframe(df[(df[col] >= min_value) & (df[col] <= max_value)])
 
-        # Graphical Visualizations
         st.subheader("Graphical Visualizations")
         plot_type = st.selectbox("Select plot type", ["Bar", "Line", "Pie"])
         selected_column = st.selectbox("Select column for plotting", df.columns)
@@ -90,7 +84,6 @@ def parse_into_anki(question_answers, deck_name):
 def main():
     st.title("Areeb's CSV to Anki Converter")
 
-    # Description box for guiding users
     st.text_area("How to Use:",
                  ("1. Take your lecture.\n"
                   "2. Fine tune a GPT to take in the material, ask it to make a CSV in a Question, Answer format for Anki Flash cards.\n"
@@ -101,16 +94,13 @@ def main():
 
     deck_name = st.text_input("Enter the name of the deck:", "Urdu 172 Vocab List 2")
 
-    # Text area for user input
     user_input = st.text_area("Enter your data (CSV format):", 
                               "Question,Answer\nSample Question,Sample Answer")
 
-    # Button to preview the input data
     if st.button('Preview Data'):
         header, data = parse_input(user_input)
         display_data(header, data)
 
-    # Button to convert the input into an Anki deck
     if st.button('Convert to Anki Deck'):
         _, question_answers = parse_input(user_input)
         if deck_name and question_answers:  # Check if deck name is not empty and data is present
